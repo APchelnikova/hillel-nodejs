ea5871c9615a50075d5e8ef3de6623c03059d473
Alina P <pchelnikovadev@gmail.com>
2026-07-21 15:38:04 +0200

    refactor: replace function to arrow

--- projects/nodejs/http-server-hw/server.js
+++ projects/nodejs/http-server-hw/server.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express();
 
 
-app.get("/", function(request, response){
+app.get("/", (request, response) => {
     response.send('Головна сторінка. Код 200 — успіх.');
 });
 
-app.get("/about", function(request, response){
+app.get("/about", (request, response) => {
     response.send('Сторінка "Про нас".');
 });
 
-app.get("/time", function(request, response){
+app.get("/time", (request, response) => {
     response.send('Поточний час: ' + new Date().toLocaleString('uk-UA'));
 });
 
-app.get("/error", function(request, response){
+app.get("/error", (request, response) => {
     response.send('Помилка сервера. Код 500.');
 });
 
-app.get("/user/:id", function (request, response) {
+app.get("/user/:id", (request, response) => {
     response.send('user ' + request.params.id);
 });
 
@@ -34,10 +34,10 @@ app.get("/user/:id/orders", (request, response) => {
     const { id } = request.params;
     const { status } = request.query;
 
-    response.send('Замовлення користувача: ' + id + ' зі статусом: ' + status);
+    response.send(`Замовлення користувача: ${id} зі статусом: ${status}`);
 });
 
-app.get("/{*any}", function(request, response){
+app.get("/{*any}", (request, response) => {
     response.send('Сторінку не знайдено. Код 404.');
 });
 
