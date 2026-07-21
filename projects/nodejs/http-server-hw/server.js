f692b594def1999c08e83c5b79b2b5fb15912571
Alina P <pchelnikovadev@gmail.com>
2026-07-21 17:07:48 +0200

    added pug template

--- projects/nodejs/http-server-hw/server.js
+++ projects/nodejs/http-server-hw/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.use(express.json());
 
 app.use(express.static('public'));
@@ -11,20 +13,6 @@ app.use((request, response, next) => {
 
     console.log(`${method} ${url} ${timestamp}`);
     next();
-});
-
-app.get('/crash', (req, res) => {
-    throw new Error('Тестова помилка');
-});
-
-app.post('/feedback', (request, response) => {
-    const { name, message } = request.body;
-
-    if(!name) {
-        return response.status(400).send('Поле name є обов\'язковим.');
-    }
-
-    response.send(`Дякуємо, ${name}! Ваш відгук отримано.`);
 });
 
 app.get("/", (request, response) =>{
@@ -55,6 +43,20 @@ app.get("/search", (request, response) => {
     response.send('Ви шукали:' + request.query.q);
 });
 
+app.get("/crash", (req, res) => {
+    throw new Error('Тестова помилка');
+});
+
+app.post("/feedback", (request, response) => {
+    const { name, message } = request.body;
+
+    if(!name) {
+        return response.status(400).send('Поле name є обов\'язковим.');
+    }
+
+    response.send(`Дякуємо, ${name}! Ваш відгук отримано.`);
+});
+
 app.get("/user/:id/orders", (request, response) => {
     const { id } = request.params;
     const { status } = request.query;
@@ -62,8 +64,29 @@ app.get("/user/:id/orders", (request, response) => {
     response.send(`Замовлення користувача: ${id} зі статусом: ${status}`);
 });
 
+app.get("/profile", (request, response) => {
+    response.render('profile', {
+        name: 'Ваше ім\'я',
+        role: 'Студент курсу Node.js',
+        isOnline: true,
+        hobbies: ['хобі 1', 'хобі 2', 'хобі 3']
+    });
+});
+
+app.get("/students", (request, response) => {
+    const students = [
+        { name: 'Оля', level: 'junior' },
+        { name: 'Максим', level: 'middle' },
+        { name: 'Ірина', level: 'senior' }
+    ];
+
+    response.render('students', { students });
+});
+
 app.use((request, response) => {
-    response.send('Сторінку не знайдено. Код 404.');
+    response.status(404).render('not-found', {
+        url: request.url
+    });
 });
 
 app.use((error, request, response, next) => {
