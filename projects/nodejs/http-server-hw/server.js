eb1014a06c2ab56a78ff1ff1e3048d5959961b7f
Alina P <pchelnikovadev@gmail.com>
2026-07-21 15:34:54 +0200

    added middleware

--- projects/nodejs/http-server-hw/server.js
+++ projects/nodejs/http-server-hw/server.js
@@ -1,24 +1,49 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 
-app.get("/", function(request, response){
+app.use(express.static('public'));
+
+app.use((request, response, next) => {
+    const { method, url } = request;
+    const timestamp = new Date().toLocaleString('uk-UA');
+
+    console.log(`${method} ${url} ${timestamp}`);
+    next();
+});
+
+app.get('/crash', (req, res) => {
+    throw new Error('Тестова помилка');
+});
+
+app.post('/feedback', (request, response) => {
+    const { name, message } = request.body;
+
+    if(!name) {
+        return response.status(400).send('Поле name є обов\'язковим.');
+    }
+
+    response.send(`Дякуємо, ${name}! Ваш відгук отримано.`);
+});
+
+app.get("/", (request, response) =>{
     response.send('Головна сторінка. Код 200 — успіх.');
 });
 
-app.get("/about", function(request, response){
+app.get("/about", (request, response) => {
     response.send('Сторінка "Про нас".');
 });
 
-app.get("/time", function(request, response){
+app.get("/time", (request, response) => {
     response.send('Поточний час: ' + new Date().toLocaleString('uk-UA'));
 });
 
-app.get("/error", function(request, response){
+app.get("/error", (request, response) => {
     response.send('Помилка сервера. Код 500.');
 });
 
-app.get("/user/:id", function (request, response) {
+app.get("/user/:id",  (request, response) => {
     response.send('user ' + request.params.id);
 });
 
@@ -34,11 +59,15 @@ app.get("/user/:id/orders", (request, response) => {
     const { id } = request.params;
     const { status } = request.query;
 
-    response.send('Замовлення користувача: ' + id + ' зі статусом: ' + status);
+    response.send(`Замовлення користувача: ${id} зі статусом: ${status}`);
 });
 
-app.get("/{*any}", function(request, response){
+app.use((request, response) => {
     response.send('Сторінку не знайдено. Код 404.');
+});
+
+app.use((error, request, response, next) => {
+    response.status(500).send('Щось пішло не так!');
 });
 
 app.listen(3000);
